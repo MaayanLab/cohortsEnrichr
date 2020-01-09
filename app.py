c6847dbfd0efbd2ceb39ce738d5181b40993c9ea
Daniel J. B. Clarke <u8sand@gmail.com>
2020-01-09 13:56:47 -0500

    Some display and control improvements

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import dash_table as dt
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -37,10 +38,37 @@ auth = dash_auth.BasicAuth(
     json.loads(os.environ.get('CREDENTIALS', '{"admin":"admin"}'))
 )
 
-app.layout = html.Div(children=[
-    html.H1(
-        children='10X Genomics Enrichment Dashboard'),
+app.index_string = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1>scEnrichr: An Enrichr interface for 10x Genomics</h1>
+                </div>
+                <div class="col-sm-12">
+                    {%app_entry%}
+                </div>
+            </div>
+        </div>
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>
+'''
 
+app.layout = html.Div(children=[
     dcc.Graph(
         id='tsne',
         figure={
@@ -55,7 +83,7 @@ app.layout = html.Div(children=[
                         line=dict(width=0.5, color='white'),
                     ),
                     text=df_tsne[df_tsne['Cluster'] == cluster]['Cluster'],
-                    name='Cluster {}'.format(cluster),
+                    name='Cluster {} '.format(cluster),
                 ) for cluster in sorted(df_tsne['Cluster'].unique())
             ],
             'layout': dict(
@@ -71,58 +99,112 @@ app.layout = html.Div(children=[
             ),
         }
     ),
-    html.Label(
-        [html.A(id='link')]
-    ),
+    html.H2(id='cluster-header'),
+    html.Label(id='enrichr-link'),
     dt.DataTable(
-        id='table',
+        id='data-table',
         columns=[
             {'name': 'cluster', 'id': 'cluster'},
             {'name': 'rank', 'id': 'rank'},
             {'name': 'direction', 'id': 'direction'},
             {'name': 'term', 'id': 'term'},
             {'name': 'category', 'id': 'category'},
-            {'name': 'pvalue', 'id': 'pvalue'},
+            {'name': 'pvalue', 'id': 'pvalue', 'type': 'numeric', 'format': { 'specifier': '.3' } },
             {'name': 'library', 'id': 'library'},
         ],
         sort_action='native',
         sort_mode='multi',
         filter_action='native',
         filter_query='{pvalue} < 0.05 && {direction} = up',
-        row_selectable='multi',
-        column_selectable='multi',
         page_action='native',
-        sort_by=[dict(column_id='category', direction='asc'),
-                 dict(column_id='rank', direction='asc')],
+        sort_by=[{ 'column_id': 'pvalue', 'direction': 'asc' }],
+        style_as_list_view=True,
+        style_header={
+            'backgroundColor': 'rgb(200, 200, 200)',
+            'fontWeight': 'bold'
+        },
+        style_table={
+            'overflowX': 'auto',
+            'width': '100%',
+            'minWidth': '100%',
+        },
+        style_data={
+            'whiteSpace': 'normal',
+            'height': 'auto',
+        },
+        css=[
+            {
+                'selector': '.dash-cell div.dash-cell-value',
+                'rule': 'display: inline; white-space: inherit; overflow: inherit; text-overflow: inherit;',
+            },
+        ],
+        style_data_conditional=[
+            {
+                'if': {'row_index': 'odd'},
+                'backgroundColor': 'rgb(230, 230, 230)'
+            }
+        ],
+        style_cell_conditional=[
+            {
+                'if': { 'column_id': 'term' },
+                'textAlign': 'left',
+                'maxWidth': '20vw',
+            },
+            {
+                'if': { 'column_id': 'category' },
+                'textAlign': 'left',
+            },
+            {
+                'if': { 'column_id': 'direction' },
+                'textAlign': 'center',
+            },
+            {
+                'if': { 'column_id': 'library' },
+                'textAlign': 'left',
+                'maxWidth': '10vw',
+            },
+        ],
     ),
 ])
 
+lock = False
+prevClickData = None
 
 @app.callback(
-    [Output('link', 'children'), Output('link', 'href')],
-    [Input('tsne', 'hoverData')]
+    [Output('cluster-header', 'children'), Output('enrichr-link', 'children'), Output('data-table', 'data')],
+    [Input('tsne', 'clickData'), Input('tsne', 'hoverData')]
 )
-def update_link_on(hoverData):
-    if not hoverData:
-        return ['', '']
-    matches = df_enrich[
-        df_enrich['cluster'] == hoverData['points'][0]['text']
-    ]
+def update_click(clickData, hoverData):
+    global lock, prevClickData
+    # Initial state
+    if not clickData and not hoverData:
+        return ['Click to cluster to select', '', []]
+    # Get relevant evt
+    if prevClickData != clickData: # Click
+        lock = not lock
+        prevClickData = clickData
+        evt = clickData
+    elif lock: # Hover but locked
+        raise PreventUpdate
+    else: # Hover and not locked
+        evt = hoverData
+    # Get cluster
+    cluster = evt['points'][0]['text']
+    matches = df_enrich[df_enrich['cluster'] == cluster]
     if matches.size == 0:
-        return ['', '']
+        return [
+            'Cluster {} ({} samples)'.format(cluster, df_tsne[df_tsne['Cluster'] == cluster].shape[0]),
+            'No data for this cluster',
+            [],
+        ]
+    # Update
     link = matches.iloc[0]['link']
-    return [link, link]
-
-
-@app.callback(
-    Output('table', 'data'),
-    [Input('tsne', 'hoverData')]
-)
-def update_table_on_hover(hoverData):
-    if not hoverData:
-        return df_enrich.to_dict('records')
-    return df_enrich[df_enrich['cluster'] == hoverData['points'][0]['text']].to_dict('records')
-
+    data = matches.to_dict('records')
+    return [
+        'Cluster {} ({} samples)'.format(cluster, df_tsne[df_tsne['Cluster'] == cluster].shape[0]),
+        ['Enrichr Link for Cluster ', html.A(link, href=link)],
+        data,
+    ]
 
 if __name__ == "__main__":
     app.run_server(
